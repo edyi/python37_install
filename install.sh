c0f354aa80ce5399875076eebefa07af96593bd3
edyi <45528050+edyi@users.noreply.github.com>
2019-12-02 18:52:35 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd /usr/local/src/
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 yum install -y zlib-devel libffi-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel libuuid-devel xz-devel
 curl -s -O https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
-tar xf Python-3.7.3.tgz && dd Python-3.7.3
+tar xf Python-3.7.3.tgz && cd Python-3.7.3
 ./configure && make && make altinstall
 
 ln -s /usr/local/bin/python3.7 /usr/bin/python3
